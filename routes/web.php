fd0fb1701fe9b2808fb6a81d1362984aa4d8bc0e
Phue Pwint Phway <phuepwintphway@gmail.com>
2024-09-13 16:54:06 +0630

    login/logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,10 +12,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// For User's Profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// For All Users
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+
+// For All 
+// Route::get('/posts')
+
 
 require __DIR__.'/auth.php';
